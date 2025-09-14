6840950e4ebb9d1e5e5027622630b05423dc9b3d
IzeLeam <luca.pourceau@gmail.com>
2025-09-14 21:06:45 +0200

    feat: add About, CyberProfile, and Projects pages with basic layout

---
+++ src/app/about/page.tsx
@@ -0,0 +1,3 @@
+export default function About() {
+    return <div className="flex items-center justify-center w-full h-full">À propos</div>;
+}
---
+++ src/app/cyber/page.tsx
@@ -0,0 +1,5 @@
+export default function CyberProfile() {
+    return (
+        <div className="flex items-center justify-center w-full h-full">Profil Cyber</div>
+    )
+}
